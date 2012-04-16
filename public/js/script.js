be4556ea1a6fef72a69663b430d8170036dbe5a7
Henning Thies <thies.henning@gmail.com>
2012-04-16 16:01:41 +0200

    testimonials

--- public/js/script.js
+++ public/js/script.js
@@ -8,4 +8,25 @@
     $(this).fadeOut("slow");
   });
   
+  $('.testimonials a').bind("hover click", function(){
+    var $elem = $(this);
+    
+    $('.testimonials li.active').removeClass("active");
+    $elem.parent().addClass("active");
+    
+    return false;
+  });
+  
+  setInterval(function(){
+    if ($(".testimonials").is(":hover")) return;
+    
+    var $active = $('.testimonials li.active');
+    if ($active.is('.testimonials li:last')) {
+      $('.testimonials li:first a').trigger("click");      
+    }else{
+      $active.next("li").find("a").trigger("click");
+    }
+    
+  }, 3000);
+  
 })(jQuery)
